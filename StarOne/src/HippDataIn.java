ccd175d082e5ddf9d081b5420cea7d2d34cd293b
Sarah <shouston2009@gmail.com>
2014-06-18 19:25:00 +0100

    parallax limit

--- StarOne/src/HippDataIn.java
+++ StarOne/src/HippDataIn.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class HippDataIn {
 
 	private final String IN_FILE = "hipp.txt";
+	private final double MIN_PARALLAX = 9;
 	private String id;
 	private double mag;
 	private double ra;
@@ -18,7 +19,7 @@ public class HippDataIn {
 	}
 
 	public void readInFile(){
-	
+
 		try{
 			FileReader reader = new FileReader(IN_FILE);
 
@@ -26,32 +27,29 @@ public class HippDataIn {
 
 
 			try{
-
-
-
 				Scanner  in = new Scanner(reader);
 				while (in.hasNextLine()){
 					String line = in.nextLine();
 					String [] tokens = line.split("[|]+");
-					
-					if (tokens[11] )
-					id = tokens[1];
-					mag = Double.parseDouble(tokens[5]);
-					ra = Double.parseDouble(tokens[8]);
-					dec = Double.parseDouble(tokens[9]);
-					par = Double.parseDouble(tokens[11]);
-					//double dis = Double.parseDouble(tokens[6]);
-					colourInd = Double.parseDouble(tokens[37]);
-					specType = tokens[76];
-					//Star test = new Star(id, ra, dec, mag, dis);
-					System.out.println(dec);
-					
-					//FileWriter out = new FileWriter("processed_data.text");
-					out.write(id + ", " + mag + ", " + ra + ", " + dec + ", " + par + ", " + colourInd + ", " + specType + ", " + "\n");
+					System.out.println(tokens[11]);
+
+					if (Double.parseDouble(tokens[11]) > MIN_PARALLAX){
+						id = tokens[1];
+						mag = Double.parseDouble(tokens[5]);
+						ra = Double.parseDouble(tokens[8]);
+						dec = Double.parseDouble(tokens[9]);
+						par = Double.parseDouble(tokens[11]);
+						//double dis = Double.parseDouble(tokens[6]);
+						colourInd = Double.parseDouble(tokens[37]);
+						specType = tokens[76];
+						//Star test = new Star(id, ra, dec, mag, dis);
+						//System.out.println("hi" + dec);
+
+						//FileWriter out = new FileWriter("processed_data.text");
+						out.write(id + ", " + mag + ", " + ra + ", " + dec + ", " + par + ", " + colourInd + ", " + specType + ", " + "\n");
+					}
 				}
 			}
-
-
 			finally{
 				if((out)!= null)
 					out.close();
